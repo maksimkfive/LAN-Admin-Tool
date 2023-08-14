b900c89c8e59268c3f9cada80652ea03d059f3bb
maksimk5 <109814868+maksimkfive@users.noreply.github.com>
2023-08-14 16:07:17 +0400

    Update database_handler.py
    reworked

--- database_handler.py
+++ database_handler.py
@@ -2,19 +2,19 @@ import sqlite3
 
 class DatabaseHandler:
     def __init__(self, db_name="network_data.db"):
-        self.conn = sqlite3.connect(db_name)
+        self.db_name = db_name
         self.create_tables()
 
     def create_tables(self):
-        with self.conn:
-            self.conn.execute("""
+        with sqlite3.connect(self.db_name) as conn:
+            conn.execute("""
                 CREATE TABLE IF NOT EXISTS Hosts (
                     id INTEGER PRIMARY KEY,
                     ip TEXT NOT NULL,
                     mac TEXT NOT NULL
                 )
             """)
-            self.conn.execute("""
+            conn.execute("""
                 CREATE TABLE IF NOT EXISTS HostDetails (
                     host_id INTEGER,
                     port INTEGER,
@@ -24,25 +24,25 @@ class DatabaseHandler:
             """)
 
     def save_host(self, ip, mac):
-        with self.conn:
-            cur = self.conn.cursor()
+        with sqlite3.connect(self.db_name) as conn:
+            cur = conn.cursor()
             cur.execute("INSERT INTO Hosts (ip, mac) VALUES (?, ?)", (ip, mac))
             return cur.lastrowid
 
     def save_host_details(self, host_id, port, service):
-        with self.conn:
-            self.conn.execute("INSERT INTO HostDetails (host_id, port, service) VALUES (?, ?, ?)",
-                              (host_id, port, service))
+        with sqlite3.connect(self.db_name) as conn:
+            conn.execute("INSERT INTO HostDetails (host_id, port, service) VALUES (?, ?, ?)",
+                         (host_id, port, service))
 
     def get_all_hosts(self):
-        with self.conn:
-            return self.conn.execute("SELECT * FROM Hosts").fetchall()
+        with sqlite3.connect(self.db_name) as conn:
+            return conn.execute("SELECT * FROM Hosts").fetchall()
 
     def get_host_details(self, host_id):
-        with self.conn:
-            return self.conn.execute("SELECT * FROM HostDetails WHERE host_id=?", (host_id,)).fetchall()
+        with sqlite3.connect(self.db_name) as conn:
+            return conn.execute("SELECT * FROM HostDetails WHERE host_id=?", (host_id,)).fetchall()
 
     def clear_cache(self):
-        with self.conn:
-            self.conn.execute("DELETE FROM HostDetails")
-            self.conn.execute("DELETE FROM Hosts")
+        with sqlite3.connect(self.db_name) as conn:
+            conn.execute("DELETE FROM HostDetails")
+            conn.execute("DELETE FROM Hosts")
